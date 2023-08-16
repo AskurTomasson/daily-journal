189e33737abcea9050a983f646eb83cfa7a582f2
AskurTomasson <askurtomasson@gmail.com>
2023-08-16 21:29:07 +0800

    initial commit for journal project

---
+++ data.js
@@ -0,0 +1,96 @@
+export const blogPosts = [
+    {
+        name: "This is just a piece of my life puzzle",
+        date: "August 9, 2023",
+        titlePost: `Self-learning has helped me so much in my life and I am now 
+                    more confident heading into new challenges in life.`,
+        fullPost: `Back in school I honestly used to suck, I just wanted to be a kid and have fun 
+                    with alot of freedom and responsibility was not something I could handle.
+                    However now, learning how to code and studying again after being a store manager for... 
+                    I dont even know how long, it's so much easier setting everything up, setting a goal and reaching it!
+                    One thing I've learned is that life experience will teach you more then any school would,
+                    You just gotta know how to read the signs that it's throwing at you and the path it's laying.
+                    `,
+        icon: `/images/puzzling.png`,
+        postId: 7,
+        header: "Puzzling"
+    },
+    {
+        name: "A well needed enviroment change",
+        date: "August 6, 2023",
+        titlePost: `When I was younger I used to travel the world,
+                    Work for some time then on to my next destination,
+                    Somewhere along the way I lost this...`,
+        fullPost: `The title might be a little missleading, but after school graduation I used to travel everywhere and work here and there.
+                    A few years down the line I stopped and settled myself for abit in Stockholm, I got good at it, at my job and with staying still
+                    but after Covid I just feelt like I needed some change so here I am in Bali, Indonesia where I think I might move to! (If I get a remote job)`,
+        icon: `/images/bali.jpeg`,
+        postId: 6,
+        header: "Get away"
+    },
+    {
+        name: "Thinking about the future",
+        date: "July 28, 2023",
+        titlePost: `I've now been learning a new skill for some time,
+                    and to be fair it has not been easy but what does the future hold 
+                    for this new skillset?`,
+        fullPost: `Coding... Oh coding... How I love you a despice you at times but you are beautiful and give me so much back that I am thankful for.
+                    I've meet some really good friends thru you and one day I hope you and I can go hand in hand fulltime.
+                    We started out 1 year ago but I've been rather shy, once we got into Javascript I fell deeply in love with your mysterious references.
+                    Just as I feel I know you so well you throw a small fight that I hope google can solve and voila we're on our way again.`,
+        icon: `/images/future.jpeg`,
+        postId: 5,
+        header: "Reminiscing"
+    },
+    {
+        name: "First sign of what to come",
+        date: "JULY 8, 2023",
+        titlePost: `Today I got a piece of reward for what I have been teaching myself 
+                    the last months, and let me tell you that feelt GOOD!`,
+        fullPost: `This here is a milestone, not for mankind but for me. Today I coded my first app for work.
+                    It was a simple function that took in a value (when scanning a barcode) and searched that value in a database,
+                    if the value existed it would throw an alert saying that this value allready exist and needs to be processed.
+                    Our IT says that I might be up for more projects in the company that I manage the store for, that would be great experience for me!`,
+        icon: `/images/positivity.jpeg`,
+        postId: 4,
+        header: "Positivity"
+    },
+    {
+        name: "As lovers go",
+        date: "June 5, 2023",
+        titlePost: `This day was full off emotions, having to watch something
+                    I cared about so deeply and fully go into the arms of someone else...`,
+        fullPost: `I've been racing in motorsports almost all my life, less then my dad wanted me to probably but I think he's happy anyway,
+                    Jokes aside today was an emotional day as I sold my racebike and I owe many thank to the people that believed in me and helped me through my racing, 
+                    wheter it be rally, crosscart or dirt biking.`,
+        icon: `/images/hurting.webp`,
+        postId: 3,
+        header: "Hurting"
+    },
+    {
+        name: "Feeling lost",
+        date: "February 21, 2023",
+        titlePost: `It could be hard learning something when you dont have a very 
+                    clear roadmap of how you are going to learn, or where to learn?`,
+        fullPost: `Learning to code has been hard, not just because coding in itself is hard but more to know where to look? And how to look at it?
+                    I started my HTML, CSS and JavaScript in freecodecamp like many others and I really do recommend that you check it out however 
+                    I feelt like I had a hard time applying it to anywhere, I would really really really recommend Scrimba to anyone who wants to learn coding. 
+                    It's the greatest studying tool that I've come upon and it makes sure you know what you're looking at.`,
+        icon: `/images/lost.jpeg`,
+        postId: 2,
+        header: "Lost"
+    },
+    {
+        name: "The beginning to a beginning",
+        date: "January 5, 2023",
+        titlePost: `In 2022 I took a decision that I wanted to step down
+                     from one of the biggest parts of my life, 
+                     and what a journey it has been.`,
+        fullPost: `On this day I decided I wanted to prioritize myself, self-growth and learning, 
+                    I took a decision to not race/compete in 2023, perhaps I'll sell my bike aswell?
+                    I'm not sure yet but I know this is for the better for my future and whatever is to come.`,
+        icon: `/images/journey1.jpeg`,
+        postId: 1,
+        header: "The start"
+    }
+]
---
+++ index.js
@@ -0,0 +1,160 @@
+import { blogPosts } from "./data.js";
+
+const showCaseDiv = document.getElementById('showcase-post-div')
+const selectedPostDiv = document.getElementById('selected-post-div')
+const whoAmIDiv = document.getElementById('who-am-i-div')
+const socialsDiv = document.getElementById('socials-div')
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+const timerId = setInterval(renderShowCaseHtml, 18000)
+let timerFunctionOne;
+let timerFunctionTwo;
+let timerFunctionThree;
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.read) {
+        myStopShowCaseFunction()
+        renderSelectedPostHtml(e.target.dataset.read)
+    }
+    if(e.target.dataset.who) {
+        renderWhoAmIHtml()
+        navMenu.classList.toggle('active')
+        hamburger.classList.toggle('active')
+    }
+    if(e.target.dataset.social) {
+        renderSocialsHtml()
+        navMenu.classList.toggle('active')
+        hamburger.classList.toggle('active')
+    }
+})
+
+function renderShowCaseHtml() {
+
+    let newestPost = blogPosts[0]
+    let secondPost = blogPosts[1]
+    let thirdPost = blogPosts[2]
+
+    timerFunctionOne = setTimeout(function(){
+        showCaseDiv.innerHTML = `
+            <img class="title-img" src="${newestPost.icon}">
+            <p class="title-date">${newestPost.date}</p>
+            <h1 class="post-title">${newestPost.name}</h1>
+            <p class="title-blog-text">${newestPost.titlePost}</p>`
+    }, 10)
+
+    timerFunctionTwo = setTimeout(function(){
+        showCaseDiv.innerHTML = `
+            <img class="title-img" src="${secondPost.icon}">
+            <p class="title-date">${secondPost.date}</p>
+            <h1 class="post-title">${secondPost.name}</h1>
+            <p class="title-blog-text">${secondPost.titlePost}</p>`
+    }, 6000)
+
+    timerFunctionThree = setTimeout(function(){
+        showCaseDiv.innerHTML = `
+            <img class="title-img" src="${thirdPost.icon}">
+            <p class="title-date">${thirdPost.date}</p>
+            <h1 class="post-title">${thirdPost.name}</h1>
+            <p class="title-blog-text">${thirdPost.titlePost}</p>`
+    }, 12000)
+
+}
+
+function myStopShowCaseFunction() {
+    clearTimeout(timerFunctionOne)
+    clearTimeout(timerFunctionTwo)
+    clearTimeout(timerFunctionThree)
+    clearInterval(timerId)
+}
+
+function renderSelectedPostHtml(postIde) {
+    const targetRenderPost = blogPosts.filter(function(post){
+        return post.postId === parseInt(postIde)
+    })[0]
+
+    showCaseDiv.style.display = 'none'
+    selectedPostDiv.style.display = 'grid'
+    whoAmIDiv.style.display = 'none'
+    socialsDiv.style.display = 'none'
+  
+    selectedPostDiv.innerHTML = `
+        <p class="selected-date">${targetRenderPost.date}</p>
+        <h1 class="selected-title">${targetRenderPost.name}</h1>
+        <p class="selected-blog-text">${targetRenderPost.titlePost}</p>
+        <img class="selected-img" src="${targetRenderPost.icon}">
+        <p class="selected-full-post">${targetRenderPost.fullPost}</p>
+        <h3 class="recent-posts">Recent posts</h3>`
+
+        window. scrollTo({top: 0, behavior: 'smooth'})
+}
+
+function renderWhoAmIHtml() {
+    showCaseDiv.style.display = 'none'
+    selectedPostDiv.style.display = 'none'
+    whoAmIDiv.style.display = 'grid'
+    socialsDiv.style.display = 'none'
+
+    whoAmIDiv.innerHTML = `
+        <img class="wai-img" src="/images/askur.png">
+        <h1 class="wai-title">Hello there! My name is Askur.</h1>
+        <p class="wai-text">I've been learning how to code for about 8 months now and it's alot of fun,
+        I currently work as a Store Operations Manager for a company called Pierce group in the motorsport industry.
+        However my goal is to switch over to front-end developing full time!</p>
+        <h2 class="wai-sub-title">My journey into the world of computers</h2>
+        <p class="wai-sub-text">It all started in primary school with my cousin wanting to get into this new hot game...
+        It was called something like World of Warcraft, we had played a little bit of other games during the early stages of internet but nothing like this.
+        My cousin who was 1 year older then me and by that time I was 11, he started making bots in Python to not go AFK in game, it would simply press a key.
+        <br>
+        <br>
+        <span class="wai-sub-footer">From there we were off to the races in computers!</span></p>
+        <h3 class="wai-recent-post">Recent posts</h3>
+        `
+}
+
+function renderSocialsHtml() {
+    showCaseDiv.style.display = 'none'
+    selectedPostDiv.style.display = 'none'
+    whoAmIDiv.style.display = 'none'
+    socialsDiv.style.display = 'grid'
+
+    socialsDiv.innerHTML = `
+        <h1 class="socials-title">You can always contact me on any of the following platforms</h1>
+        <a href="https://www.facebook.com/karlhenrik.gyllenkrans/" class="fa-brands fa-facebook-f"></a>
+        <a href="https://www.instagram.com/askur.63/" class="fa-brands fa-instagram"></a>
+        <a href="https://www.linkedin.com/in/askur-tomasson-b72327253/" class="fa-brands fa-linkedin-in"></a>
+        <h3 class="socials-recent-post">Recent posts</h3>`
+}
+
+
+function blogFeed() {
+    
+    let blogFeedHtml = ``
+
+    blogPosts.forEach(function(post){
+
+        blogFeedHtml += `
+        <div class="blog-card" id="blog-card">
+            <img class="blog-card-img" data-read="${post.postId}" src="${post.icon}">
+            <p class="blog-card-date">${post.date}</p>
+            <h2 class="blog-card-title">${post.header}</h2>
+            <p class="blog-card-post">${post.titlePost}</p>
+        </div>
+        `
+    })
+    return blogFeedHtml
+
+
+}
+
+function renderBlogFeed() {
+    document.getElementById('blog-feed-div').innerHTML = blogFeed()
+}
+
+renderBlogFeed()
+renderShowCaseHtml()
+
